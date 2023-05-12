ce1ecbb433ec2df4859a1af2288b2d3b1e7c2e46
kaloyan-gavrilov <kgavrilov08@gmail.com>
2023-05-12 10:11:44 +0300

    final
    added more sections to the website and added new images

--- home-script.js
+++ home-script.js
@@ -9,5 +9,20 @@ const observer = new IntersectionObserver((entries) => {
     })
 })
 
+const v_observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('v_show');
+        } else {
+            entry.target.classList.remove('v_show');
+        }
+    })
+})
+
 const hiddenElements = document.querySelectorAll('.hidden');
+const v_hiddenElements = document.querySelectorAll('.v_hidden');
+
 hiddenElements.forEach((el) => observer.observe(el));
+v_hiddenElements.forEach((el) => v_observer.observe(el));
+
